41f35f42bc3814511ed18a29055d2596a845d998
John Berg <21scout0311@gmail.com>
2013-12-17 20:47:09 -0500

    adding some missing files

---
+++ Unity-Project/JUNIOR V1/Assets/Scripts/GunController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+    GameObject guns;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void Rotate()
+    {
+
+    }
+}
---
+++ Unity-Project/JUNIOR V1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    
+
+    void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(Screen.width / 2 - 64, Screen.height / 2 - 128, 256, 250));
+
+        GUI.Box(new Rect(0, 0, 256, 512), "J.U.N.I.O.R.");
+
+        if (GUI.Button(new Rect(256 / 2 - 40, 40, 80, 30), "Play"))
+        {
+            Application.LoadLevel("Grounds001");
+        }
+        if (GUI.Button(new Rect(256 / 2 - 40, 40, 80, 30), "Credits"))
+        {
+            //add credits here
+        }
+        if (GUI.Button(new Rect(256 / 2 - 40, 80, 80, 30), "Exit"))
+        {
+            Application.Quit();
+        }
+
+        GUI.EndGroup();
+    }
+}
