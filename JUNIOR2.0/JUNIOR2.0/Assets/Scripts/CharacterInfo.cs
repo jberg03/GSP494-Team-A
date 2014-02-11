d6069b20f462355a21956b159b7907f814889285
shadowangles <shadowangles@aol.com>
2014-02-10 20:04:39 -0600

    Character Info Scripts
    Created a base character info script as well as a corresponding player
    info script

---
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterInfo : MonoBehaviour 
+{
+	public float health = 30;
+	protected float maxHealth;
+	public float movementSpeed = 3.0f;
+	public float rotationSpeed = 1.0f;
+	protected StatusCondition condition;
+
+	void Start()
+	{
+		maxHealth = health;
+	}
+
+	public bool IsLowHealth()
+	{
+		if(((health / maxHealth) * 100) > 20)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	public float GetMovementSpeed()
+	{
+		//check status
+		return movementSpeed;
+	}
+	
+	public float GetRotationSpeed()
+	{
+		return rotationSpeed;
+	}
+	
+	public void DealDamage(float damage)
+	{
+		health -= damage;
+	}
+	
+	public bool IsDead()
+	{
+		if(health <= 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public void ApplyStatus(StatusCondition statCondition)
+	{
+		if(statCondition != null)
+		{
+			
+		}
+	}
+}
--- JUNIOR2.0/JUNIOR2.0/Assets/Scripts/Enemy/EnemyInfo.cs
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/Enemy/EnemyInfo.cs
@@ -1,67 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
-
-
-public class EnemyInfo : MonoBehaviour
+public class EnemyInfo : CharacterInfo
 {
-	public float health = 30;
-	protected float maxHealth;
-	protected StatusCondition condition;
 	public float reachTarget = 5.0f;
 	public float seeTarget = 25.0f;
-	public float movementSpeed = 3.0f;
-	public float rotationSpeed = 1.0f;
-
 	public GameObject platform;
-
-
-
-	void Start()
-	{
-		maxHealth = health;
-	}
-
-	public float GetMovementSpeed()
-	{
-		//check status
-		return movementSpeed;
-	}
-
-	public float GetRotationSpeed()
-	{
-		return rotationSpeed;
-	}
-
-	public bool IsLowHealth()
-	{
-		if(((health / maxHealth) * 100) > 20)
-		{
-			return false;
-		}
-		return true;
-	}
-
-	public void DealDamage(float damage)
-	{
-		health -= damage;
-	}
-
-	public bool IsDead()
-	{
-		if(health <= 0)
-		{
-			return true;
-		}
-		return false;
-	}
-
-	public void ApplyStatus(StatusCondition statCondition)
-	{
-		if(statCondition != null)
-		{
-
-		}
-	}
 }
 
