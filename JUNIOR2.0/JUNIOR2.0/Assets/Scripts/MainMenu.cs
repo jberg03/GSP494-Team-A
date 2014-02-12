de6b089dd569455889d08880547abaddd6befec6
John Berg <21scout0311@gmail.com>
2014-02-11 23:34:20 -0500

    main menu

---
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, 160, 150, 75), "PLAY"))
+            Application.LoadLevel("Level01");
+        
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, 250, 150, 75), "QUIT"))
+            Application.Quit();
+    }
+}
+
