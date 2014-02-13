0030c09d65a8e5d933c3403fb679dafae3c4d5d7
shadowangles <shadowangles@aol.com>
2014-02-13 07:14:58 -0800

    added a script to be able to play sounds during the animation

---
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/EffectScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectScript : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		AudioSource.PlayClipAtPoint (audio.clip, this.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
