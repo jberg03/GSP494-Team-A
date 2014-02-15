498bb402dc2430fa6a02fe49ea982f24c10e81f9
shadowangles <shadowangles@aol.com>
2014-02-15 06:57:52 -0800

    adds the dead state

---
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/FSM/AdvancedFSM/DeadState.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class DeadState : FSMState
+{
+
+	public AudioClip sound;
+	public GameObject effect;
+
+	public override void Construct ()
+	{
+		this.stateId = FSMStateId.Dead;
+		if(this.transform.GetComponent<EnemyInfo>().platform != null)
+		{
+			this.DetermineBoundries ();
+		}
+	}
+
+	void Update()
+	{
+		if(this.transform.GetComponent<EnemyInfo>().platform != null)
+		{
+			this.DetermineBoundries ();
+		}
+	}
+
+	public override void Act (Transform player)
+	{
+		//status update
+		Debug.Log ("Enemy is about to explode");
+		AudioSource.PlayClipAtPoint (sound, this.transform.position);
+		Destroy(Instantiate(this.effect, this.transform.position, this.transform.rotation),7.0f);
+		Destroy (gameObject);
+
+	}
+}
+
