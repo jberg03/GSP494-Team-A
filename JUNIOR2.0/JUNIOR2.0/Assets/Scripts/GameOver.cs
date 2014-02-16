9f57bc815683b896d8045c7e585a1c2aebc1b2d1
John Berg <21scout0311@gmail.com>
2014-02-16 16:36:55 -0500

    Game Over Screen

---
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+    public Texture gameOverTexture;
+
+    void OnStart()
+    {
+        gameOverTexture = new Texture();
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2 - 300, 30, 600, Screen.height - 60), gameOverTexture, GUIStyle.none);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, 300, 150, 75), "REPLAY"))
+            Application.LoadLevel("Level01");
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, 390, 150, 75), "MAIN MENU"))
+            Application.LoadLevel("MainMenu");
+    }
+}
