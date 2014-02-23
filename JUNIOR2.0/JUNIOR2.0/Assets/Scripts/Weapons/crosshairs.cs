d5e75f66cd4430259d5c36c201891ba6f8d3117e
shadowangles <shadowangles@aol.com>
2014-02-22 16:31:48 -0800

    modified to aim at the bullet spawnpoint at the crosshairs

--- JUNIOR2.0/JUNIOR2.0/Assets/Scripts/Weapons/crosshairs.cs
+++ JUNIOR2.0/JUNIOR2.0/Assets/Scripts/Weapons/crosshairs.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class crosshairs : MonoBehaviour {
 
@@ -11,5 +12,14 @@ public class crosshairs : MonoBehaviour {
 		float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
 		float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
 		GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+	}
+
+	void FixedUpdate()
+	{
+		foreach(Transform spawnPoint in this.transform.GetComponentsInChildren<Transform>().Where(point => point.name.ToLower() == "bulletspawnpoint"))
+		{
+			Transform[] mainCamera = this.transform.parent.GetComponentsInChildren<Transform>().Where(child => child.name == "Main Camera").ToArray();
+			spawnPoint.rotation = mainCamera[0].rotation;
+		}
 	}
 }
